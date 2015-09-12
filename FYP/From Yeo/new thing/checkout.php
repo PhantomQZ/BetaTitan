c8fb6292482274288b44c98bc5f5c8a6d3192a51
ae861995 <ae861995@hotmail.com>
2015-09-12 22:52:52 +0800

    new

---
+++ FYP/From Yeo/new thing/checkout.php
@@ -0,0 +1,112 @@
+<?php
+session_start(); //start session
+include("conn.php"); //include config file
+$lastno = mysqli_query($conn, "SELECT * FROM cart ORDER BY Order_ID DESC LIMIT 1");
+$rows = mysqli_fetch_assoc($lastno);
+?>
+<?php
+	if (isset($_GET['process'])){
+	$total 			= 0;
+	$list_tax 		= '';
+	$cart_box 		= '<ul class="view-cart">';
+	$lastnum=0;
+	
+	foreach($_SESSION["products"] as $product){ //Print each item, quantity and price.
+		$product_name = $product["Game_name"];
+		$product_price = $product["Game_price"];
+		$product_code = $product["Game_Register_ID"];
+		
+		$item_price 	= sprintf("%01.2f",($product_price));  // price x qty = total item price
+		
+		$cart_box 		.=  "<li> $product_code &ndash;  $product_name <span> $currency. $item_price </span></li>";
+		
+		$subtotal 		= ($product_price ); //Multiply item quantity * price
+		$total 			= ($total + $subtotal); //Add up to total price
+		if ($rows['Order_ID']!=NULL)
+		{
+		$lastnum=$rows['Order_ID']+1;
+		$query ="Insert into cart(Order_ID, Game_name, Game_price) values ('$lastnum', '$product_name', '$product_price') ";
+		mysqli_query($conn,$query);
+		}
+		else
+		{
+		$lastnum=1;
+		$query ="Insert into cart(Order_ID, Game_name, Game_price) values ('$lastnum', '$product_name', '$product_price') ";
+		mysqli_query($conn,$query);	
+		}
+	}
+		$user_check = $_SESSION['login_user'];    
+		$userid = mysqli_fetch_assoc(mysqli_query($conn,"select User_ID from user where User_usrname like '$user_check'"));
+		$uid = $userid['User_ID'];
+		$date = date('Y-m-d');
+		$query ="Insert into payment(Order_ID, User_ID, Price, Payment_date) values ('$lastnum', '$uid', '$total', '$date') ";
+		mysqli_query($conn,$query);	
+			unset($_SESSION["products"]);		 
+	}
+?>
+<?php
+$user_check = $_SESSION['login_user'];    
+$userid = mysqli_fetch_assoc(mysqli_query($conn,"select * from user where User_usrname like '$user_check'"));
+$uid = $userid['User_ID'];
+$ufname= $userid['User_Fname'];
+$ulname= $userid['User_Lname'];
+$a = "select * from user where User_ID ='$uid'";
+//$a = "select * from user where User_email='$mail' AND User_usrname='$usrname' ";
+$q = mysqli_query($conn, $a);
+while($res=mysqli_fetch_assoc($q)){
+
+$cname=$res['User_usrname'];
+$uemail=$res['User_email'];
+require 'PHPMailerAutoload.php';
+require 'class.smtp.php';
+require 'class.phpmailer.php';
+
+$mail = new PHPMailer;
+ 
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'titiangaming123@gmail.com';            // SMTP username
+$mail->Password = 'Titiangaming.123';               		  // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+$mail->Port = 465;                                    //Set the SMTP port number - 587 for authenticated TLS
+$mail->setFrom('titiangaming123@gmail.com', 'Titan_Gaming');     //Set who the message is to be sent from
+//$mail->addReplyTo('cdpcyb.test@gmail.com', 'Cdp Cyberjaya');  //Set an alternative reply-to address
+$mail->AddAddress("$uemail");     // Add a recipient
+//$mail->addAddress('mereka@example.com');              // Name is optional
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+$mail->addAttachment('c:/Attachment/head-xampp.gif');         // Add attachments
+$mail->addAttachment('c:/Attachment/The PHP.docx', 'The Php'); // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = "Your Shopping Cart @ Titan Gaming";
+$mail->AddEmbeddedImage('C:/xampp/htdocs/IT/images/BetaTitan.png', 'logoimg', 'BetaTitan.png');
+$mail->Body    = "<html>
+<body style=\"margin: 10px;\">
+<div style=\"width: 640px; font-family: Arial, Helvetica, sans-serif; font-size: 15px;\">
+<div align=\"center\"><p><img src=\"cid:logoimg\" /></p></div><br>
+<br>
+
+Hello, $ulname $ufname <br><br>
+
+<br> The game will be send to you after the Admin check the validity of the bank in receipt.
+<br> If the payment have no been made for three days, the order will automatically be reject.
+<br> Please bank in the receipt in the link below.
+<a href='http://localhost/ajaxstore/receipt.php?OID=$lastnum' style='font-size=15pt;'>Make Your Payment Here</a>
+<br/>
+
+</div>
+</body>
+</html>";
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+  echo "Message has been sent";
+}
+}
+echo "<script language=\"javascript\">alert('An email has been sent to your inbox for payment. Redirecting you to main page.');
+					window.location.href='d2.php';
+			 </script>";		
+?>
---
+++ FYP/From Yeo/new thing/view_cart.php
@@ -0,0 +1,166 @@
+<?php
+session_start(); //start session
+include("conn.php");
+setlocale(LC_MONETARY,"en_US"); // US national format (see : http://php.net/money_format)
+?>
+<!DOCTYPE HTML>
+<head> 
+	<title>Titian Online Game Store</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link href="style/style.css" rel="stylesheet" type="text/css">
+	<link rel ="stylesheet" type="text/css" href="main.css"/>
+	<link href="style/style.css" rel="stylesheet" type="text/css">
+
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="jquery.cycle.all.min.js"></script>
+	<script type="text/javascript" src="scripts.js"></script>
+	<style>
+	body{
+	
+	background-color: black;
+	background-image: url(background.jpg);
+	background-position: top;
+	width:100%;
+	
+	
+}
+	#header{
+	background: #191919 url(/borderless/images/default/bg_header.png) repeat-x center top;
+	height: 140px;
+	margin-top: -10px;
+	margin-left: -11px;
+	
+}
+		#header a.logo{no-repeat 5px top;
+	display: block;
+	float: left;
+	height: 137px;
+	width: 80px
+}
+		#header h1{display:none}
+		#menu{
+	float: left;
+	list-style: none;
+	margin-top: 48px;
+	margin-right: 0;
+	margin-left: 358px;
+	margin-bottom: 0;
+	margin-left: 317px
+}
+		#menu li{display:block;float:left;height:60px;}
+		#menu li a{display:block;color:#7BD2DC;float:left;font-size:19px;font-weight:bold;height:60px;line-height:60px;padding:0 8px;text-align:center;text-decoration:none}
+		#menu:hover li.on a:hover,
+		#menu li a:hover,
+		#menu li.on a{background:url(/borderless/images/default/bg_header_menu.png) no-repeat center bottom;_background:none;color:#00FFF3}
+		#menu li.platforms a:hover{background:none}
+		#menu:hover li.on a{background:none}
+		#menu li a.menutooltip{background:#080808;color:#fff;line-height:1;padding:6px;text-indent:0;width:auto;z-index:2}
+		#login{float:right;padding-right:10px;text-align:right;width:370px;color:#FFFFFF}
+		#login a{color:#FFFFFF;text-decoration:none}
+	</style>
+</head>
+	<div id="header">
+		<div class="">
+		<h1>Titan Games</h1>
+		<a href="main.php" class="logo" title="Home"><img src="BetaTitan.png" height="140"/></a>
+		<ul id="menu">
+		<li><a href="#">Games</a></li>
+		<li><a href="#">Community</a></li>
+		<li><a href="AboutUs.php">About Us</a></li>
+		<li><a href="#">Support</a></li>
+        <li><a href="developer.php">Developer</a></li>
+		</ul>
+		</div>
+	</div>
+	<div id="content">
+		<div class="container">
+			<div id="member">
+				<div id="login">
+					<span>
+							<?php
+							$link1 = 'RegisterUser.php';
+							$link2 = 'loginpage.php';
+							$link3 = 'logout.php';
+							$link4 = 'edit_profile.php';
+							if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+							{
+								
+								echo "Welcome &nbsp;";
+								echo $_SESSION['login_user'];
+								echo "&nbsp;/&nbsp;";
+								echo "<a href = '".$link4."'>Edit Profile</a>";
+								echo "&nbsp;/&nbsp;";
+								echo "<a href = '".$link3."'>Log Out</a>";
+							}
+							else
+								{
+								echo "Welcome &nbsp;/&nbsp; </i>";
+								echo "<a href='".$link1."'>Register</a>&nbsp;/&nbsp;";
+								echo "<a href='".$link2."'>Login</a>";
+								}
+							?>
+					</span>
+				</div>
+			</div>
+		</div>
+ </div>
+<body>
+<br><br>
+<h3 style="text-align:center;color:white;font-size:20pt;">Review Your Cart Before Buying</h3>
+<?php
+if(isset($_SESSION["products"]) && count($_SESSION["products"])>0){
+	if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+							{
+	$total 			= 0;
+	$list_tax 		= '';
+	$cart_box 		= '<ul class="view-cart">';
+
+	foreach($_SESSION["products"] as $product){ //Print each item, quantity and price.
+		$product_name = $product["Game_name"];
+		$product_price = $product["Game_price"];
+		$product_code = $product["Game_Register_ID"];
+		
+		$item_price 	= sprintf("%01.2f",($product_price));  // price x qty = total item price
+		
+		$cart_box 		.=  "<li> $product_code &ndash;  $product_name <span> $currency. $item_price </span></li>";
+		
+		$subtotal 		= ($product_price ); //Multiply item quantity * price
+		$total 			= ($total + $subtotal); //Add up to total price
+	}
+	
+	$grand_total = $total + $shipping_cost; //grand total
+	
+	foreach($taxes as $key => $value){ //list and calculate all taxes in array
+			$tax_amount 	= round($total * ($value / 100));
+			$tax_item[$key] = $tax_amount;
+			$grand_total 	= $grand_total + $tax_amount; 
+	}
+	
+	foreach($tax_item as $key => $value){ //taxes List
+		$list_tax .= $key. ' '. $currency. sprintf("%01.2f", $value).'<br />';
+	}
+	
+	$shipping_cost = ($shipping_cost)?'Shipping Cost : '.$currency. sprintf("%01.2f", $shipping_cost).'<br />':'';
+	
+	//Print Shipping, VAT and Total
+	$cart_box .= "<li class=\"view-cart-total\">$shipping_cost  $list_tax <hr>Payable Amount : $currency ".sprintf("%01.2f", $grand_total)."</li>";
+	$cart_box .= "&nbsp;";
+	$cart_box .= '<li style="float:right;font-size:18pt;""><u><a href="checkout.php?process=true" title="Proceed to payment">Check-out</a></u></li>';
+	$cart_box .= "</ul>";
+	
+	echo $cart_box;
+	}
+	else
+	{
+		unset($_SESSION["products"]);
+		echo "<script language=\"javascript\">alert('Error Please login again.');
+		window.location.href='d2.php';
+		 </script>";
+	}
+	
+}else{
+	echo "Your Cart is empty";
+}
+?>
+</body>
+</html>
